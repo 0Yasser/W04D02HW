3b3d51099415b9fb4ff7962f519672878b7ebebb
KhaledAlhamad <khaledalhamad997@gmail.com>
2021-10-25 16:52:48 +0300

    first commit

---
+++ w04d02hw/src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  let twitter=[
+    {
+        name: "khaled",
+        image:"",
+        tweets: ["Hello Twitter","Follow me"]
+    },
+    {
+        name: "Yasser",
+        image:"",
+        tweets: ["I've just learn coding","Visca Barca"]
+    },
+    {
+        name: "Mohammed",
+        image:"",
+        tweets: ["Thank you"]
+    },
+]
+
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ w04d02hw/src/components/Tweet.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const Tweet = (props) => {
+    console.log(props)
+    
+    return (
+        <h1> Hello {props.name} </h1>
+    )
+}
+
+export default Tweet  
